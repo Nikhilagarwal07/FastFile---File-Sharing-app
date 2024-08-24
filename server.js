8358b364dc936f53aa4df34e753c9840bea248e3
Nikk-07 <f20201611@pilani.bits-pilani.ac.in>
2024-08-24 23:36:08 +0530

    added cors

--- server.js
+++ server.js
@@ -3,15 +3,23 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
 
+
 const connectDB = require('./config/db');
 connectDB();
 
+
+//cors
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOptions))
 //template engine ejs configure
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine','ejs');
